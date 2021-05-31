90b4396f45cdfa23e621868f4021a38e9cbdee1f
alainb-debug <a.bouchaud@codeur.online>
2021-05-31 15:45:12 +0200

    feat:projet fini

---
+++ add.js
@@ -0,0 +1,26 @@
+
+const text = document.querySelector('h2');
+
+
+function getChrono(){
+    const now = new Date() . getTime();
+const countdownDate = new Date ('December 1, 2021').getTime();
+    const distanceBase = countdownDate - now;
+
+const days = Math.floor(distanceBase / (1000 * 60 * 60 * 24));
+const hours = Math. floor((distanceBase % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+
+const minutes = Math.floor((distanceBase % (1000 * 60 * 60)) / (1000 * 60))
+const seconds = Math.floor((distanceBase % (1000 * 60)) / 1000);
+
+text.innerText = `${days}j ${hours}h ${minutes}m ${seconds}s` ;
+
+}
+getChrono()
+
+
+
+const countDownInterval = setInterval(() => {
+    getChrono()
+
+} , 1000);
